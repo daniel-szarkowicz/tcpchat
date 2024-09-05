7c8b26099bad703e83fa96653ae035d3b437c64c
Szarkowicz Dániel <szarkowicz.dani@proton.me>
2024-09-05 20:23:35 +0200

    rewrite Connection::receive to use an owned buffer (remove read_exact)

--- common/src/connection.rs
+++ common/src/connection.rs
@@ -2,19 +2,24 @@ use std::io::{Cursor, Error, ErrorKind, Read, Result, Write};
 use std::marker::PhantomData;
 use std::net::TcpStream;
 use std::ops::{Deref, DerefMut};
-use std::time::{Duration, Instant};
 
 use super::Codec;
-
-const READ_TIMEOUT: Duration = Duration::from_millis(1000);
 
 #[derive(Debug)]
 pub struct Connection<Sent: Codec + ?Sized, Received: Codec + ?Sized> {
     stream: TcpStream,
-    buffer: Vec<u8>,
     _sent: PhantomData<Sent>,
     _received: PhantomData<Received>,
-    timeout: Option<Instant>,
+
+    read_buffer: Vec<u8>,
+    read_buffer_actual: usize,
+    read_mode: ReadMode,
+}
+
+#[derive(Debug)]
+enum ReadMode {
+    Size,
+    Data,
 }
 
 impl<Sent: Codec + ?Sized, Received: Codec + ?Sized>
@@ -24,52 +29,53 @@ impl<Sent: Codec + ?Sized, Received: Codec + ?Sized>
         stream.set_nonblocking(true)?;
         Ok(Self {
             stream,
-            buffer: vec![],
             _sent: PhantomData,
             _received: PhantomData,
-            timeout: None,
+
+            read_buffer: vec![0; size_of::<u16>()],
+            read_buffer_actual: 0,
+            read_mode: ReadMode::Size,
         })
     }
 
     pub fn receive(&mut self) -> Result<Received::Owned> {
-        if self.buffer.is_empty() {
-            let mut buf = [0; size_of::<u16>()];
-            read_exact(&mut self.stream, &mut buf, &mut self.timeout)?;
-            let data_size = u16::from_be_bytes(buf);
-            self.buffer.resize(data_size as usize, 0);
+        let n = self
+            .stream
+            .read(&mut self.read_buffer[self.read_buffer_actual..])?;
+        if n == 0 {
+            return Err(Error::from(ErrorKind::UnexpectedEof));
         }
-        debug_assert!(!self.buffer.is_empty());
-        read_exact(&mut self.stream, &mut self.buffer, &mut self.timeout)?;
-        Received::decode(&mut Cursor::new(std::mem::take(&mut self.buffer)))
+        self.read_buffer_actual += n;
+        if self.read_buffer_actual == self.read_buffer.len() {
+            match self.read_mode {
+                ReadMode::Size => {
+                    let data_size =
+                        u16::decode(&mut Cursor::new(&mut self.read_buffer))
+                            .unwrap_or_else(|_| unreachable!());
+                    self.read_buffer.resize(data_size as usize, 0);
+                    self.read_buffer_actual = 0;
+                    self.read_mode = ReadMode::Data;
+                    self.receive()
+                }
+                ReadMode::Data => {
+                    let result = Received::decode(&mut Cursor::new(
+                        &mut self.read_buffer,
+                    ));
+                    self.read_buffer.resize(2, 0);
+                    self.read_buffer_actual = 0;
+                    self.read_mode = ReadMode::Size;
+                    result
+                }
+            }
+        } else {
+            Err(Error::from(ErrorKind::WouldBlock))
+        }
     }
 
     pub fn send(&mut self, msg: &Sent) -> Result<()> {
         let data_size = msg.coded_size() as u16;
         self.stream.write_all(&data_size.to_be_bytes())?;
         msg.code(&mut self.stream)
-    }
-}
-
-// TODO: move into `Connection`
-fn read_exact(
-    stream: &mut TcpStream,
-    buf: &mut [u8],
-    timeout: &mut Option<Instant>,
-) -> Result<()> {
-    match stream.peek(buf)? {
-        n if n == buf.len() => stream.read_exact(buf),
-        0 => Err(Error::new(
-            ErrorKind::ConnectionAborted,
-            "read 0 bytes from stream",
-        )),
-        _ => {
-            let timeout = timeout.get_or_insert_with(Instant::now);
-            if timeout.elapsed() > READ_TIMEOUT {
-                Err(Error::from(ErrorKind::TimedOut))
-            } else {
-                Err(Error::from(ErrorKind::WouldBlock))
-            }
-        }
     }
 }
 
